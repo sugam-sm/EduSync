339c1da6209c74fe49cb3f5e3ba65d1bd4e126ee
Sugam Shrestha <sugamshrestha.sm@gmail.com>
2026-03-24 18:39:00 +0545

    modified nav items for smooth animations and hover effects.

--- frontend/src/components/navItem.tsx
+++ frontend/src/components/navItem.tsx
@@ -11,20 +11,19 @@ export const NavButton = ({ to, label, icon }: NavButtonProps) => (
   <NavLink
     to={to}
     className={({ isActive }) =>
-      `flex flex-col lg:flex-row w-13.75 lg:w-full items-center justify-center gap-1/2 lg:gap-1 px-1 lg:px-3 py-1 lg:py-1.5 rounded-md lg:rounded-full font-semibold transition-all duration-100 active:translate-y-0.5  ${
-        isActive
-          ? 'bg-primary text-text-heading' 
-          : 'text-text-muted hover:text-text-heading hover:bg-primary/50'
+      `flex flex-col lg:flex-row w-13.75 lg:w-full items-center justify-center gap-1/2 lg:gap-1 px-0.5 lg:px-2.5 py-1 lg:py-1.5 rounded-md lg:rounded-full font-semibold transition-all duration-200 active:translate-y-0.5  ${isActive
+        ? 'bg-primary text-text-heading'
+        : 'text-text-muted hover:text-text-heading hover:bg-primary/50 hover:delay-[100ms]'
       }`
     }
   >
     {({ isActive }) => (
       <>
         <span className="shrink-0 transition-all duration-300">
-          {React.isValidElement(icon) 
-            ? React.cloneElement(icon as React.ReactElement<any>, { 
-                strokeWidth: isActive ? 2.5 : 2 
-              }) 
+          {React.isValidElement(icon)
+            ? React.cloneElement(icon as React.ReactElement<any>, {
+              strokeWidth: isActive ? 2.5 : 2
+            })
             : icon}
         </span>
         
